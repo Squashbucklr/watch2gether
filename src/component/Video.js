784b2fe11b458d84a710e894ebd986aa554ea519
Squashbucklr <2jbwar22@gmail.com>
2021-03-01 01:16:19 -0500

    added prototype audio slider

--- src/component/Video.js
+++ src/component/Video.js
@@ -19,19 +19,23 @@ class Video extends React.Component {
     constructor(props) {
         super(props);
         this.needsTimeChange = true;
+        this.needsAudioChange = true;
         this.needsSourceLoad = false;
         this.mouseTimeout = null;
         this.videoNode = React.createRef();
         this.videoWrapper = React.createRef();
         this.videoScrubber = React.createRef();
+        this.videoAudioScrubber = React.createRef();
+        this.audio_memory = 0;
         this.state = {
             seeking: false,
-            hover_scrubber: false,
-            hover_play: false,
-            hover_audio: false,
-            hover_fulscreen: false,
-            video_currentTime: 0,
+            seekingAudio: false,
+            videoOverlayHovers: [false, false, false, false, false, false, false],
+            video_currentTimeStamp: 0,
+            video_currentTimeFrac: 0,
+            video_duration: 0,
             video_fullscreen: false,
+            video_audio_level: 1,
             hideMouse: true,
             peek: {
                 left: 0,
@@ -46,6 +50,7 @@ class Video extends React.Component {
 
     shouldComponentUpdate = (nextProps, nextState) => {
         this.needsTimeChange = Math.abs(this.props.time - nextProps.time) > 0.01;
+        this.needsAudioChange = Math.abs(this.state.video_audio_level - nextState.video_audio_level) > 0.001;
         this.needsSourceLoad = this.props.url !== nextProps.url;
         return true;
     }
@@ -53,6 +58,7 @@ class Video extends React.Component {
     componentDidUpdate = () => {
         if (this.needsSourceLoad) this.videoNode.current.load();
         if (this.needsTimeChange) this.fixVideoPosition();
+        if (this.needsAudioChange) this.fixAudioValue();
         if (this.videoNode.current.paused && this.props.play) {
             this.videoNode.current.play();
         } else if (!this.videoNode.current.paused && !this.props.play) {
@@ -66,7 +72,17 @@ class Video extends React.Component {
             // important to note that this.props.time only changes when the websocket passes the video state.
             // it does not live update.
             this.videoNode.current.currentTime = this.props.time;
+            let frac = this.props.time / this.getDuration();
+            if (isNaN(frac)) frac = 0;
+            this.setState({
+                video_currentTimeStamp: this.getTime(this.props.time),
+                video_currentTimeFrac: frac
+            })
         }
+    }
+
+    fixAudioValue = () => {
+        this.videoNode.current.volume = this.state.video_audio_level;
     }
 
     scrubberPeek = (e) => {
@@ -75,6 +91,15 @@ class Video extends React.Component {
         if (scrubThru <= 0) scrubThru = 0;
         if (scrubThru >= 1) scrubThru = 1;
         this.scrubFrac(scrubThru);
+        
+    }
+
+    scrubberAudioPeek = (e) => {
+        let scrubBox = this.videoAudioScrubber.current.getBoundingClientRect();
+        let scrubThru = 1 - ((e.pageY - scrubBox.y) / (scrubBox.height - 5));
+        if (scrubThru <= 0) scrubThru = 0;
+        if (scrubThru >= 1) scrubThru = 1;
+        this.scrubAudioFrac(scrubThru);
         
     }
 
@@ -87,17 +112,27 @@ class Video extends React.Component {
         });
     }
 
+    scrubAudioFrac = (frac) => {
+        this.setState({
+            peekAudio: {
+                value: frac
+            }
+        });
+    }
+
     mouseHide = (time) => {
         return (e) => {
             clearTimeout(this.mouseTimeout);
             this.setState({hideMouse: false});
             this.mouseTimeout = setTimeout(() => {
-                if(
-                    !this.state.hover_scrubber && 
-                    !this.state.hover_play &&
-                    !this.state.hover_audio &&
-                    !this.state.hover_fulscreen
-                ) {
+                let show = false;
+                for (const zone of this.state.videoOverlayHovers) {
+                    if (zone) {
+                        show = true;
+                        break;
+                    }
+                }
+                if(!show) {
                     this.setState({hideMouse: true});
                 }
             }, time);
@@ -122,6 +157,16 @@ class Video extends React.Component {
         this.setState({seeking: false});
     }
 
+    doSeekAudio = () => {
+        if(this.state.seekingAudio) {
+            if (this.state.peekAudio.value === 0) {
+                this.audio_memory = this.state.video_audio_level;
+            }
+            this.setState({video_audio_level: this.state.peekAudio.value})
+        }
+        this.setState({seekingAudio: false});
+    }
+
     getCurrentTime = () => {
         return this.videoNode ? this.videoNode.current ? this.videoNode.current.currentTime : 0 : 0;
     }
@@ -142,16 +187,34 @@ class Video extends React.Component {
 
     handleFullscreen = () => {
         if (this.isFullScreen()) {
-           if (document.exitFullscreen) document.exitFullscreen();
-           else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
-           else if (document.webkitCancelFullScreen) document.webkitCancelFullScreen();
-           else if (document.msExitFullscreen) document.msExitFullscreen();
+            if (document.exitFullscreen) document.exitFullscreen();
+            else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+            else if (document.webkitCancelFullScreen) document.webkitCancelFullScreen();
+            else if (document.msExitFullscreen) document.msExitFullscreen();
         }
         else {
-           if (this.videoWrapper.current.requestFullscreen) this.videoWrapper.current.requestFullscreen();
-           else if (this.videoWrapper.current.mozRequestFullScreen) this.videoWrapper.current.mozRequestFullScreen();
-           else if (this.videoWrapper.current.webkitRequestFullScreen) this.videoWrapper.current.webkitRequestFullScreen();
-           else if (this.videoWrapper.current.msRequestFullscreen) this.videoWrapper.current.msRequestFullscreen();
+            if (this.videoWrapper.current.requestFullscreen) this.videoWrapper.current.requestFullscreen();
+            else if (this.videoWrapper.current.mozRequestFullScreen) this.videoWrapper.current.mozRequestFullScreen();
+            else if (this.videoWrapper.current.webkitRequestFullScreen)
+                this.videoWrapper.current.webkitRequestFullScreen();
+            else if (this.videoWrapper.current.msRequestFullscreen) this.videoWrapper.current.msRequestFullscreen();
+        }
+    }
+
+    handleVideoOverlayHover = (zone, hover) => {
+        let newVideoOverlayHovers = [...this.state.videoOverlayHovers];
+        newVideoOverlayHovers[zone] = hover;
+        this.setState({videoOverlayHovers: newVideoOverlayHovers});
+    }
+
+    handleMute = (e) => {
+        if (e.target == e.currentTarget) {
+            if (this.state.video_audio_level > 0) {
+                this.audio_memory = this.state.video_audio_level;
+                this.setState({video_audio_level: 0});
+            } else {
+                this.setState({video_audio_level: this.audio_memory});
+            }
         }
     }
 
@@ -171,7 +234,13 @@ class Video extends React.Component {
                     <video
                         ref={this.videoNode}
                         className="Video-video"
-                        onTimeUpdate={() => {this.setState({video_currentTime: this.getCurrentTime()})}}
+                        onTimeUpdate={() => {
+                            this.setState({
+                                video_currentTimeStamp: this.getTime(this.getCurrentTime()),
+                                video_currentTimeFrac: this.getCurrentTime() / this.getDuration()
+                            })
+                        }}
+                        onDurationChange={() => {this.setState({video_duration: this.getDuration()})}}
                     >
                         <source src={this.props.url}></source>
                     </video>
@@ -180,28 +249,67 @@ class Video extends React.Component {
                     >
                         <div
                             className="Video-play"
-                            onMouseOver={() => {this.setState({hover_play: true})}}
-                            onMouseOut={() => {this.setState({hover_play: false})}}
+                            onMouseOver={() => {this.handleVideoOverlayHover(1, true)}}
+                            onMouseOut={() => {this.handleVideoOverlayHover(1, false)}}
                             onClick={() => {this.props.playPause(this.getCurrentTime())}}
                         ><FontAwesomeIcon icon={this.props.play ? faPause : faPlay} /></div>
                         <div
                             className="Video-audio"
-                            onMouseOver={() => {this.setState({hover_audio: true})}}
-                            onMouseOut={() => {this.setState({hover_audio: false})}}
-                        ><FontAwesomeIcon icon={faVolumeMute} /></div>
+                            onMouseOver={() => {this.handleVideoOverlayHover(2, true)}}
+                            onMouseOut={() => {this.handleVideoOverlayHover(2, false)}}
+                            onClick={this.handleMute}
+                        >
+                            <FontAwesomeIcon className="Video-hide-icon" icon={
+                            this.state.video_audio_level > 0.5 ? faVolumeUp :
+                                this.state.video_audio_level === 0 ? faVolumeMute : faVolumeDown} />
+                            <div
+                                ref={this.videoAudioScrubber}
+                                className="Video-audio-scrubber"
+                                onMouseMove={this.scrubberAudioPeek}
+                                onMouseEnter={() => {this.handleVideoOverlayHover(6, true)}}
+                                onMouseLeave={() => {
+                                    this.handleVideoOverlayHover(6, false);
+                                    this.doSeekAudio();
+                                }}
+                                onMouseDown={() => {
+                                    this.setState({seekingAudio: true});
+                                }}
+                                onMouseUp={() => {
+                                    this.doSeekAudio();
+                                }}
+                            >
+                                <div className="Video-audio-scrubber-bg"></div>
+                                <div
+                                    className="Video-audio-scrubber-bar"
+                                    style={{
+                                        height: (this.state.video_audio_level * 100) + '%'
+                                    }}
+                                ></div>
+                            </div>
+                        </div>
                         <div
                             className="Video-fullscreen"
-                            onMouseOver={() => {this.setState({hover_fullscreen: true})}}
-                            onMouseOut={() => {this.setState({hover_fullscreen: false})}}
+                            onMouseOver={() => {this.handleVideoOverlayHover(3, true)}}
+                            onMouseOut={() => {this.handleVideoOverlayHover(3, false)}}
                             onClick={this.handleFullscreen}
                         ><FontAwesomeIcon icon={this.isFullScreen() ? faCompress : faExpand} /></div>
+                        <div
+                            className="Video-time-through"
+                            onMouseOver={() => {this.handleVideoOverlayHover(4, true)}}
+                            onMouseOut={() => {this.handleVideoOverlayHover(4, false)}}
+                        >{this.state.video_currentTimeStamp}</div>
+                        <div
+                            className="Video-time-total"
+                            onMouseOver={() => {this.handleVideoOverlayHover(5, true)}}
+                            onMouseOut={() => {this.handleVideoOverlayHover(5, false)}}
+                        >{this.getTime(this.state.video_duration)}</div>
                         <div
                             ref={this.videoScrubber}
                             className="Video-scrubber"
                             onMouseMove={this.scrubberPeek}
-                            onMouseEnter={() => {this.setState({hover_scrubber: true})}}
+                            onMouseEnter={() => {this.handleVideoOverlayHover(0, true)}}
                             onMouseLeave={() => {
-                                this.setState({hover_scrubber: false});
+                                this.handleVideoOverlayHover(0, false);
                                 this.doSeek();
                             }}
                             onMouseDown={() => {
@@ -215,7 +323,7 @@ class Video extends React.Component {
                             <div
                                 className="Video-scrubber-bar"
                                 style={{
-                                    width: ((this.state.video_currentTime / this.getDuration()) * 100) + '%'
+                                    width: (this.state.video_currentTimeFrac * 100) + '%'
                                 }}
                             ></div>
                             <div className="Video-scrubber-time"
